e42aae174ad49065784f78d491591e71032bafa6
cospan <cospan@gmail.com>
2013-12-21 13:22:18 -0500

    Added defines for colorizing the output

---
+++ include/print_colors.hpp
@@ -0,0 +1,14 @@
+#ifndef __PRINT_COLOR_HPP__
+#define __PRINT_COLOR_HPP__
+
+#define P_GRAY          "\x1b[30m"
+#define P_RED           "\x1b[31m"
+#define P_GREEN         "\x1b[32m"
+#define P_YELLOW        "\x1b[33m"
+#define P_BLUE          "\x1b[34m"
+#define P_MAGENTA       "\x1b[35m"
+#define P_CYAN          "\x1b[36m"
+#define P_NORMAL        "\x1b[0m"
+
+#endif //__PRINT_COLOR_HPP__
+
